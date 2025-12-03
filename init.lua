126b3b894a55c6141df2a29d86eb16543fe8034e
WilsonLessley14 <wilsonlessley14@gmail.com>
2025-12-02 17:27:11 -0800

    feat: change to snacks.nvim and avante

--- init.lua
+++ init.lua
@@ -29,61 +29,176 @@ vim.g.maplocalleader = "\\"
 
 local plugins = {
   {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { "f-person/git-blame.nvim", event = "VeryLazy" },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
+    "folke/snacks.nvim",
+    priority = 1000,
     lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      bigfile = { enabled = true },
+      terminal = { 
+        enabled = true,
+        win = { style = "terminal" }
+      },
+      dashboard = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      lazygit = { enabled = true, configure = true },
+      words = { enabled = true },
     },
-    config = function()
-      require("nvim-tree").setup {}
-    end,
+    keys = {
+      -- file picker/explorer
+      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      -- lazygit
+      { "<leader>lg", function() Snacks.lazygit() end, desc = "LazyGit" },
+    },
   },
   {
-    "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-    lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-    dependencies = {
-      -- main one
-      { "ms-jpq/coq_nvim", branch = "coq" },
-
-      -- 9000+ Snippets
-      { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-      -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-      -- Need to **configure separately**
-      { 'ms-jpq/coq.thirdparty', branch = "3p" }
-      -- - shell repl
-      -- - nvim lua api
-      -- - scientific calculator
-      -- - comment banner
-      -- - etc
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- this file can contain specific instructions for your project
+      instructions_file = "avante.md",
+      -- for example
+      provider = "openai",
+      providers = {
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-5",
+          timeout = 60000, -- Timeout in milliseconds, increase this for reasoning models
+          context_window = 128000, -- Number of tokens to send to the model for context
+          extra_request_body = {
+            temperature = 1,
+            reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          },
+        },
+      },
+      mappings = {
+        submit = {
+          insert = "<CR>"
+        }
+      },
+      rag_service = { -- RAG Service configuration
+        enabled = false, -- Enables the RAG service
+        host_mount = os.getenv("HOME"), -- Host mount path for the rag service (Docker will mount this path)
+        runner = "docker", -- Runner for the RAG service (can use docker or nix)
+        llm = { -- Language Model (LLM) configuration for RAG service
+          provider = "openai", -- LLM provider
+          endpoint = "https://api.openai.com/v1", -- LLM API endpoint
+          api_key = "OPENAI_API_KEY", -- Environment variable name for the LLM API key
+          model = "gpt-4o-mini", -- LLM model name
+          extra = nil, -- Additional configuration options for LLM
+        },
+        embed = { -- Embedding model configuration for RAG service
+          provider = "openai", -- Embedding provider
+          endpoint = "https://api.openai.com/v1", -- Embedding API endpoint
+          api_key = "OPENAI_API_KEY", -- Environment variable name for the embedding API key
+          model = "text-embedding-3-large", -- Embedding model name
+          extra = nil, -- Additional configuration options for the embedding model
+        },
+        docker_extra_args = "", -- Extra arguments to pass to the docker command
+      },
     },
-    init = function()
-      vim.g.coq_settings = {
-          auto_start = true, -- if you want to start COQ at startup
-          -- Your COQ settings here
-      }
-    end,
-    config = function()
-      -- Your LSP settings here
-    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   },
-}
-local opts = {
-  install = { colorscheme = { "habamax" } },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+    config = function()
+        require("mcphub").setup()
+    end
+  }
 }
 
 require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
+require("avante").setup({
+  -- system_prompt as function ensures LLM always has latest MCP server state
+  -- This is evaluated for every message, even in existing chats
+  system_prompt = function()
+      local hub = require("mcphub").get_hub_instance()
+      return hub and hub:get_active_servers_prompt() or ""
+  end,
+  -- Using function prevents requiring mcphub before it's loaded
+  custom_tools = function()
+      return {
+          require("mcphub.extensions.avante").mcp_tool(),
+      }
+  end,
+  -- MCP HUB uses neovims built-server tools for file operations and server access. avante provides its own.
+  -- i have to disable one or the other to avoid conflicts
+  -- i'm opting to use the default neovim server tools
+  disabled_tools = {
+    "list_files",    -- Built-in file operations
+    "search_files",
+    "read_file",
+    "create_file",
+    "rename_file",
+    "delete_file",
+    "create_dir",
+    "rename_dir",
+    "delete_dir",
+    "bash",         -- Built-in terminal access
+  },
+})
 
 --------------------------
 -------- OPTIONS ---------
@@ -101,5 +216,3 @@ vim.api.nvim_set_keymap('n', '<leader>w', '<c-w>', { noremap = true })
 -- leave :term mode with ESC
 vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { noremap = true})
 
-
-vim.cmd.colorscheme "habamax"
